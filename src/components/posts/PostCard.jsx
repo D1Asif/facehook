fa7a4e79e228850a67823d4ce9b5b4e47aa5db24
Dewan Asifur Rahman <dasifrahman@gmail.com>
2024-03-15 18:36:58 +0600

    post id removed

--- src/components/posts/PostCard.jsx
+++ src/components/posts/PostCard.jsx
@@ -7,7 +7,6 @@ import PostHeader from "./PostHeader";
 export default function PostCard({post}) {
   return (
     <article className="card mt-6 lg:mt-8">
-      <p>{post?.id}</p>
         <PostHeader post={post} />
         <PostBody poster={post?.image} content={post?.content} />
         <PostAction post={post} commentCount={post?.comments.length} />
